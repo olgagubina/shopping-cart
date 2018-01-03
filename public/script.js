270f410310620d19a8812e44e0810b0efd45ad84
olgagubina <oagubina@gmail.com>
2018-01-03 10:30:02 +0300

    added addItem function

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@
 var cart = [];
 
 var updateCart = function () {
+
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
@@ -9,6 +10,8 @@ var updateCart = function () {
 
 
 var addItem = function (item) {
+  cart.push(item);
+  console.log(cart);
   // TODO: Write this function. Remember this function has nothing to do with display. 
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
 }
@@ -22,6 +25,12 @@ $('.view-cart').on('click', function () {
 });
 
 $('.add-to-cart').on('click', function () {
+  var name = $(this).closest('div').parent().data().name;
+  var price = $(this).closest('div').parent().data().price;
+  var item = {
+    name: name,
+    price: price
+  }
   // TODO: get the "item" object from the page
   addItem(item);
   updateCart();
